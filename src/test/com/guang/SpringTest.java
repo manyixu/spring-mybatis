d8ab1d430715ab13cc4ca3eb2eb62365996d8b02
leeguang <liyg1011@outlook.comgit>
2016-02-26 11:23:06 +0800

    aa

--- src/test/com/guang/SpringTest.java
+++ src/test/com/guang/SpringTest.java
@@ -2,8 +2,10 @@ package guang;
 
 import com.guang.daomain.User;
 import com.guang.service.UserService;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -14,6 +16,7 @@ import java.util.List;
  * Created by admin on 2016/2/22.
  */
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringJUnit4ClassRunner.class)        //表示继承了SpringJUnit4ClassRunner类
 @ContextConfiguration(locations = {"classpath:spring-myBatis.xml"})
 public class SpringTest {
@@ -22,11 +25,6 @@ public class SpringTest {
     private UserService userService;
 
     public void test1() {
-        List<User> users = userService.getUsers();
-        System.out.println(users.size());
-    }
-
-    public void test2() {
         for (int i = 0; i < 100; i++) {
             User user = new User();
             user.setPhone("1350000000" + i);
@@ -39,12 +37,54 @@ public class SpringTest {
         }
     }
 
-    @Test
+    public void test2() {
+        List<User> users = userService.getUsers();
+        System.out.println(users.size());
+    }
+
     public void test3() {
         User user = new User();
         user.setPhone("13500000002");
         user.setUserName("小强");
         System.out.println(userService.updateUsersByPhone(user));
     }
+
+
+
+    @Test
+    public void test4(){
+        int x = 5;//瓶盖剩余
+        int y = 5;//瓶的剩余
+        int z = 5;//总瓶数
+        while (x >= 4 || y >= 2){
+            if(x >= 4){
+                int t = (int) Math.floor(x / 4);
+                z = z + t;
+                x = x % 4;
+                y = y + t;
+                x = x + t;
+                System.out.println("x:" + x + "---y:" + y +"---z:" + z + "---t:" + t);
+            }
+            if(y >= 2){
+                int t = (int) Math.floor(y / 2);
+                z = z + t;
+                y = y % 2;
+                x = x + t;
+                y = y + t;
+                System.out.println("x:" + x + "---y:" + y +"---z:" + z + "---t:" + t);
+            }
+        }
+        System.out.println(z);
+
+    }
+
+
+
+
+
+
+
+
+
 
 }
